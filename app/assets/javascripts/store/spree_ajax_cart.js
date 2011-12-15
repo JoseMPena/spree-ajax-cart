2ae9a17fb6dcb8524a47e34a4d04a6b0cd8e47c6
Roman Simecek <roman.simecek@cyt.ch>
2011-12-15 12:12:57 +0100

    Submit the amout with the ajax link.

--- app/assets/javascripts/store/spree_ajax_cart.js
+++ app/assets/javascripts/store/spree_ajax_cart.js
@@ -5,7 +5,16 @@
       $(this).parents('form').submit();
       return false;
     });
+
     $('#add-to-cart-ajax-link').show();
     $('#add-to-cart-button').hide();
+
+    $('#add-to-cart-ajax-link').click(function(){
+      var amount = $('input[name^="variants"]:first').val();
+
+      $(this).attr('href', function(i, h) {
+        return h + (h.indexOf('?') != -1 ? "&amount=" : "?amount=") + amount;
+      });
+    });
   });
 })(jQuery);
