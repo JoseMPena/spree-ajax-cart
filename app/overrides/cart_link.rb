fbcb1135339cdab6fc1f28f59d4aa9907718da70
Roman Simecek <roman.simecek@cyt.ch>
2012-01-04 10:15:00 +0100

    Optimize the deface replacement of the cart link.

--- app/overrides/cart_link.rb
+++ app/overrides/cart_link.rb
@@ -1,6 +1,4 @@
 Deface::Override.new(:virtual_path => "spree/products/_cart_form",
                      :name =>         'add_to_cart_ajax',
-                     :insert_before => 'code[erb-loud]:contains("add-to-cart-button")',
-                     :text => '<%= link_to "/cart/add/#{@product.master.id}", :remote => true, :method => :put, :id => "add-to-cart-ajax-link" do %>
-                                <%= image_tag("icons/add-to-cart.png") + t(:add_to_cart) %>
-                              <% end %>')
+                     :replace_contents => 'code[erb-loud]:contains("add-to-cart-button")',
+                     :text => 'link_to "/cart/add/#{@product.master.id}", :remote => true, :method => :put, :id => "add-to-cart-ajax-link" do')
