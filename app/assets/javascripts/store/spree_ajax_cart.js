4ecaf443075ea53ead58f1b15d8100b76a8a7955
Roman Simecek <roman.simecek@cyt.ch>
2012-04-05 11:58:02 +0200

    Fix id name.

--- app/assets/javascripts/store/spree_ajax_cart.js
+++ app/assets/javascripts/store/spree_ajax_cart.js
@@ -1,6 +1,6 @@
 (function($){
   $(document).ready(function(){
-		$("form#updatecart a.delete").live("click", function(){
+		$("form#update-cart a.delete").live("click", function(){
       $(this).parents('tr').find('input.line_item_quantity').val(0);
       $(this).parents('form').submit();
       return false;
